577fa9eec85806f58a6f2d18564c5b929f709697
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2018-12-16 00:16:38 +0300

    part1  half

--- Homework3/Part1/depthfirstsearch.py
+++ Homework3/Part1/depthfirstsearch.py
@@ -1,60 +1,54 @@
+import xlrd
+import datetime
+import traceback
+from collections import defaultdict
 
-# Python program to print DFS traversal for complete graph 
-from collections import defaultdict 
-  
-# This class represents a directed graph using adjacency 
-# list representation 
-class Graph: 
-  
-    # Constructor 
-    def __init__(self): 
-  
-        # default dictionary to store graph 
-        self.graph = defaultdict(list) 
-  
-    # function to add an edge to graph 
-    def addEdge(self,u,v): 
-        self.graph[u].append(v) 
-  
-    # A function used by DFS 
-    def DFSUtil(self, v, visited): 
-  
-        # Mark the current node as visited and print it 
-        visited[v]= True
-        print (v)
-  
-        # Recur for all the vertices adjacent to 
-        # this vertex 
-        for i in self.graph[v]: 
-            if visited[i] == False: 
-                self.DFSUtil(i, visited) 
-  
-  
-    # The function to do DFS traversal. It uses 
-    # recursive DFSUtil() 
-    def DFS(self): 
-        V = len(self.graph)  #total vertices 
-  
-        # Mark all the vertices as not visited 
-        visited =[False]*(V) 
-  
-        # Call the recursive helper function to print 
-        # DFS traversal starting from all vertices one 
-        # by one 
-        for i in range(V): 
-            if visited[i] == False: 
-                self.DFSUtil(i, visited) 
-  
-  
-# Driver code 
-# Create a graph given in the above diagram 
-g = Graph() 
-g.addEdge(0, 1) 
-g.addEdge(0, 2) 
-g.addEdge(1, 2) 
-g.addEdge(2, 0) 
-g.addEdge(2, 3) 
-g.addEdge(3, 3) 
-  
-print ("Following is Depth First Traversal")
-g.DFS() 
+
+visited_dfs = [] 
+visited_bfs = [] 
+
+graph = {}
+def readGraphFromWorksheet():
+    workbook = xlrd.open_workbook('F:/GitKrakenRepository/CSE_321/CSE_321/Homework3/Part1/Graph_data.xls')
+    worksheet = workbook.sheet_by_name('Sheet1')
+
+    rootVertex = int(worksheet.cell(0, 1).value)
+    print("root vertex:", rootVertex,"\n")
+
+    for row_index in range(3, worksheet.nrows):
+        _from = int(worksheet.row(row_index)[0].value)
+        _to = int(worksheet.row(row_index)[1].value)
+        if _from in graph:
+            graph[_from].append(_to)
+        else:
+            graph[_from] = [_to]
+    
+    return (rootVertex, graph)
+
+def dfs(graph,node):    
+    if node not in visited_dfs:
+        visited_dfs.append(node)
+        for n in graph[node]:
+            dfs(graph,n)
+    return visited_dfs
+    
+def bfs(graph, start):
+    visited_bfs, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited_bfs:
+            visited_bfs.add(vertex)
+            queue.extend(graph[vertex] - visited_bfs)
+    return visited_bfs
+    
+if __name__ == "__main__":
+    result = readGraphFromWorksheet()
+    try:
+        visited = dfs(graph,4)
+    except Exception as exception:
+        print("Depth-First Search (DFS) result")
+        print(visited_dfs)   
+        print("Breadth-First Search (BFS) result")
+        print(visited_bfs)
+        print(readGraphFromWorksheet())
+    pass
--- Homework3/Part1/depthfirstsearch1.py
+++ Homework3/Part1/depthfirstsearch1.py
@@ -1,6 +1,6 @@
 import xlrd
-import datetime
 import traceback
+from collections import defaultdict
 
 workbook = xlrd.open_workbook(
     'F:/GitKrakenRepository/CSE_321/CSE_321/Homework3/Part1/Graph_data.xls')
@@ -9,7 +9,7 @@ worksheet = workbook.sheet_by_name('Sheet1')
 print("First Column", "Second Coulmn")
 
 try:
-    index = 0
+    index = 3
     rowCount = 0
     graph = {}
     firstColumn = worksheet.cell(index, 0).value
@@ -18,15 +18,17 @@ try:
     while worksheet.cell(index, 0).value != None:
         firstColumn = worksheet.cell(index, 0).value
         secondColumn= worksheet.cell(index, 1).value
-        if index == 0:
-            root_node = secondColumn
-        if index >= 3:
-            if graph[firstColumn] == None:
-                graph[firstColumn] = secondColumn
-            elif graph[firstColumn] != None:
-                graph[firstColumn] = graph[firstColumn].append(secondColumn)
+        if graph[firstColumn] == None:
+            graph[firstColumn].add(secondColumn)
+        elif graph[firstColumn] != None:
+            tempList = list()
+            tempList.append(graph[firstColumn])
+            tempList.append(secondColumn)                
+            graph[firstColumn] = tempList
+        print (index)        
         rowCount += 1
         index += 1
 except Exception as exception:
+    print(exception)
     for keys, values in graph.items():
         print(keys, values)
